08fa97be2271a4a41a611f98284e0bc547b4984d
Phil Barber <phil.barber93@gmail.com>
2022-12-04 17:16:34 +0000

    day4.1

---
+++ day4/solution.py
@@ -0,0 +1,35 @@
+AssignmentPair = tuple[str, str]
+
+
+class Assignment:
+    def __init__(self, assignment: str) -> None:
+        start, end = assignment.split("-")
+        self.start = int(start)
+        self.end = int(end)
+
+    def __repr__(self) -> str:
+        return f"[{self.start}, {self.end}]"
+
+    def contains(self, other: "Assignment") -> bool:
+        return self.start <= other.start and self.end >= other.end
+
+
+def assignment_contians_or_contained_in(one: Assignment, other: Assignment) -> bool:
+    return one.contains(other) or other.contains(one)
+
+
+def main(assignment_pairs: list[AssignmentPair]) -> int:
+    assignments = [(Assignment(a), Assignment(b)) for a, b in assignment_pairs]
+    return sum(assignment_contians_or_contained_in(*pair) for pair in assignments)
+
+
+def input_lines() -> list[str]:
+    with open("input") as input_file:
+        return list(input_file.readlines())
+
+
+if __name__ == "__main__":
+    inputs = input_lines()
+    inputs = [tuple(line[:-1].split(",")) for line in inputs if line[0]]
+    answer = main(inputs)
+    print(f"Answer: {answer}")
---
+++ day4/test_solution.py
@@ -0,0 +1,41 @@
+import pytest
+
+from solution import (Assignment, AssignmentPair,
+                      assignment_contians_or_contained_in, main)
+
+example_input = [
+    ("2-4", "6-8"),
+    ("2-3", "4-5"),
+    ("5-7", "7-9"),
+    ("2-8", "3-7"),
+    ("6-6", "4-6"),
+    ("2-6", "4-8"),
+]
+
+
+def test_main_example() -> None:
+    assert main(example_input) == 2
+
+
+@pytest.mark.parametrize(
+    "assignment_pair, expected",
+    [
+        (example_input[0], False),
+        (example_input[1], False),
+        (example_input[2], False),
+        (example_input[3], True),
+        (example_input[4], True),
+        (example_input[5], False),
+        (("51-51", "52-68"), False),
+        (("66-66", "9-65"), False),
+    ],
+)
+def test_assignment_contains_or_contained_in_assignment(
+    assignment_pair: AssignmentPair, expected: bool
+) -> None:
+    assert (
+        assignment_contians_or_contained_in(
+            Assignment(assignment_pair[0]), Assignment(assignment_pair[1])
+        )
+        == expected
+    )
